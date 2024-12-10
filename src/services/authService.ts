e726d28e7d2e7837e308293e0a99e5bee46f971a
mhndkptr <muhamadhendika77@gmail.com>
2024-12-11 01:54:38 +0700

    chore: update jwt error class

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -4,14 +4,14 @@ import { IUser } from "../interfaces/userInterface";
 import { ILoginData } from "../interfaces/authInterface";
 import { loginSchema } from "../validation/authSchema";
 import User from "../models/userModel";
-const { ValidationError, AuthenticationError } = require("../middlewares/errorHandler");
+const { ValidationError, AuthenticationError, InternalServerError } = require("../middlewares/errorHandler");
 require("dotenv").config();
 
 const generateUserToken = (user: IUser) => {
   const secret = process.env.APP_JWT_SECRET;
 
   if (!secret) {
-    throw new Error("JWT secret is not defined in the environment variables");
+    throw new InternalServerError("JWT secret is not defined in the environment variables");
   }
 
   return jwt.sign({ id: user.id }, secret, {
