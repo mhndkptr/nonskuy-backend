71654b634a8e7ef3624efce620e83f7c97f3a66c
mhndkptr <muhamadhendika77@gmail.com>
2024-12-14 17:36:14 +0700

    chore: update credentials

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
     origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
+    credentials: false,
   })
 );
 
